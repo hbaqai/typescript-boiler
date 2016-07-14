0124cb7ffe5126f40b15eac51fac885ab97bf849
Hashir Baqai <hashirbaqai@gmail.com>
2016-07-14 02:47:01 -0700

    adding Angular 2's tslint.json rules

--- typescriptboiler.sh
+++ typescriptboiler.sh
@@ -12,11 +12,8 @@ npm install --save tslint typescript@beta typings -f
 #create .vscode dir if it doesn't exist
 mkdir -p .vscode
 
-#curl to get settings.json from gist
-#echo '// Place your settings in this file to overwrite default and user settings.' > ./.vscode/settings.json
-#echo '{' >> ./.vscode/settings.json
-#echo '    "typescript.tsdk": "./node_modules/typescript/lib"' >> ./.vscode/settings.json
-#echo '}' >> ./.vscode/settings.json
+#install node typings definition
+node_modules/.bin/typings install env~node --global --save
 
 #remove .git file so user can initialize this as a new git repo
 #rm -rf .git
